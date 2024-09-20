48832ebf3cd663d15dc7af6c6c30c6b9815a0645
Farquad <jake@xyzdigital.com>
2024-09-20 12:37:13 -0700

    setting up tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,10 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        'dark-bg': '#121212',
+        'darker-bg': '#282828',
+      },
       fontFamily: {
         sans: [
           '"Inter"',
